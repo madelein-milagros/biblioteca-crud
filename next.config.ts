9e8ed7aaa480856ebf10790b11580facbec3cb53
madelein-milagros <ramoschamorromilagros@gmail.com>
2026-06-01 18:37:10 -0500

    fix: incluir binario

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/.prisma/client/**/*"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
